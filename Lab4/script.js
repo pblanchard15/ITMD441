a6d87e793785bf68b5c15f0e694dcf8809e23a4c
pblanchard15 <pblanchard@hawk.iit.edu>
2022-11-18 12:38:41 -0600

    Add data to javascript file and create search bar for region section

---
+++ Lab4/script.js
@@ -0,0 +1,16 @@
+{
+    "region": "Chicago, IL",
+    "currentConditions": {
+    "dayhour": "Friday 7:00 PM",
+    "temp": {
+    "f": 27
+    },
+    "precip": "10%",
+    "humidity": "30%",
+    "wind": {
+    "mile": 18
+    },
+    "iconURL": "https://th.bing.com/th/id/R.146cb8a155e50ceffe11c9deb978317c?rik=CJiZ%2b4tJETewxQ&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2010%2f09%2fsymbol-weather-windy-clip-art_285983.png&ehk=nYh7AzIlwK9SVpDYw0IgwTy0hMQn2cRZ8eMDvVO6BHY%3d&risl=&pid=ImgRaw&r=0",
+    "comment": "Windy"
+    }
+}
